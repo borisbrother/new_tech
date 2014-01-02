86662990b6e87ea835b512fbe789d2551541ed12
Boris Hasanov <bo1@yandex.ru>
2014-01-02 22:03:12 +0300

    Добавление динамичного меню на javascript в файл index.html через файл js/main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 window.onload = function() {
-	
+	var mylist = document.getElementById("mylist");
+	for (i = 1; i < 10; i++) {
+		var li = document.createElement('li');
+		var a = document.createElement('a');
+		a.setAttribute('href', '#');
+		a.onclick = function(q) { 
+			return function(e) {
+				e.preventDefault();
+				alert(q); 
+			}
+		}(i);
+		a.innerHTML = "Меню " + i;
+		li.appendChild(a);
+		mylist.appendChild(li);
+	}
 }
