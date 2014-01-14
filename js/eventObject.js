53e68e11965967e2339a306144ab2a2c7a1b7dab
Boris Hasanov <bo1@yandex.ru>
2014-01-14 17:39:46 +0300

    Последние изменения

--- js/eventObject.js
+++ js/eventObject.js
@@ -1,4 +1,5 @@
 var eventObject = {
+
 	addEvent: function(elem, method, func) {
 
 		if (elem.addEventListener) {
@@ -20,3 +21,64 @@ var eventObject = {
 		}
 	}
 };
+
+var person = {
+	name : "Николай",
+	age : 23,
+	gender : 1
+};
+
+
+
+
+Object.prototype.call = function() {};
+
+// for (i in person) {	
+// 	if (Object.prototype.hasOwnProperty.call(person, i)) {
+// 		// console.log(i, ":", person[i]);
+// 	}	
+// }
+
+var func = function() {
+	var arguments = Array.slice.call(this, arguments);
+	var myFunc = arguments.shift();
+	// console.log(arguments.reduce(myFunc));
+};
+
+func(function(a, b) {
+	return a + b;
+}, 2,3,4,5);
+
+
+
+var objectReg = {
+	reg: "",
+	dist: "",
+	replace: function(el) {
+		el.innerHTML = el.innerHTML.replace(this.reg, this.dist);
+	}
+};
+
+var toLink = document.getElementsByTagName('p');
+objectReg.reg = /(\w*te\w*)/ig;
+objectReg.dist = "<a href='$1'>$1</a>";
+
+for (i = 0, max = toLink.length; i < max; i++) {
+	objectReg.replace.call(objectReg, toLink[i]);
+	//toLink[i].innerHTML = toLink[i].innerHTML.replace(/(\w*si\w*\s)/ig, "<a href='#'>$1</a>"); 
+	//toLink[i].innerHTML.replace(/(Lorem)/g, "Boris");
+}
+
+var house = ({
+	width: 25,
+	height:50,
+	getSquare: function() {
+		return this.width * this.height;
+	},
+	init: function() {
+		console.log(this.getSquare());
+		return this;
+	}
+}).init();
+
+console.log(2 * house.getSquare());
--- js/main.js
+++ js/main.js
@@ -1,29 +1,28 @@
 window.onload = function() {
 	var mylist = document.getElementById("mylist");
-	for (i = 1; i < 10; i++) {
-		var li = document.createElement('li');
-		var a = document.createElement('a');
-		a.setAttribute('href', '#');
-		// a.onclick = function(q) { 
-		// 	return function(e) {
-		// 		e.preventDefault();
-		// 		alert(q); 
-		// 	}
-		// }(i);
-		a.onclick = function () {
-			var q = i;
-			return function cl() {
-				alert(q);
-			} 			
-		}();
-		a.innerHTML = "Меню " + i;
-		li.appendChild(a);
-		mylist.appendChild(li);
+	if (mylist !== null) {
+		for (i = 1; i < 10; i++) {
+			var li = document.createElement('li'),
+				a = document.createElement('a');
+			a.setAttribute('href', '#');
+			a.onclick = function(q) { 
+				return function(e) {
+					e.preventDefault();
+					alert(q); 
+				}
+			}(i);
+			
+			a.innerHTML = "Меню " + i;
+			li.appendChild(a);
+			
+			mylist.appendChild(li);
+		}
+		
 	}
 
 
-	var buttons = document.getElementsByTagName('button');
-	var karkas = document.getElementById("karkas");
+	var buttons = document.getElementsByTagName('button'),
+		karkas = document.getElementById("karkas");
 	var changeColor = function(event) {
 		var target = event.target || event.srcElement;
 		event.preventDefault ? event.preventDefault() : event.returnValue =  false;
@@ -37,13 +36,11 @@ window.onload = function() {
 		document.body.className = target.id;
 	}
 
-	// karkas.addEventListener('click', function() {
-	// 	alert("Hello");
-	// }, false);
+	
 
 	for (i = 0, len = buttons.length; i < len; i++) {
 		eventObject.addEvent(buttons[i], "click", changeColor);
 	}
 
-	eventObject.removeEvent(buttons[0], "click", changeColor);
+	//eventObject.removeEvent(buttons[0], "click", changeColor);
 } 
